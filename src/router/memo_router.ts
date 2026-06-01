8292095edb7e93695d9323508a5338022497a692
najongjine <najongjin3@hotmail.com>
2026-06-01 11:17:46 +0900

    .

--- src/router/memo_router.ts
+++ src/router/memo_router.ts
@@ -4,20 +4,25 @@ import { query } from "../db/pool.js";
 const router = new Hono();
 
 router.get("/", async (c) => {
-  const result = await query(`
+  try {
+    const result = await query(`
       SELECT 
-        id,
-        name,
-        email,
-        created_at
-      FROM users
+       *
+      FROM t_memo
       ORDER BY id DESC
     `);
 
-  return c.json({
-    success: true,
-    data: result.rows,
-  });
+    return c.json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (e: any) {
+    return c.json({
+      success: false,
+      data: null,
+      msg: `!server error. ${e?.message}`,
+    });
+  }
 });
 
 export default router;
