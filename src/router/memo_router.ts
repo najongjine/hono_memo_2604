fb08191a9a5b18e7966a35a7e3f527a38a788f6f
najongjine <najongjin3@hotmail.com>
2026-06-04 11:35:38 +0900

    .

--- src/router/memo_router.ts
+++ src/router/memo_router.ts
@@ -52,52 +52,36 @@ router.get("/select_by_id", async (c) => {
   }
 });
 
-router.post("/insert_memo", async (c) => {
-  try {
-    const body = await c.req.parseBody();
-    const title = String(body["title"] || "");
-    const content = String(body["content"] || "");
-    const nickname = String(body["nickname"] || "");
-    const result = await query(
-      `
-    INSERT INTO t_memo(title,content,nickname)
-    VALUES($1,$2,$3)
-    `,
-      [title, content, nickname],
-    );
-    console.log(`#result:`, result);
-
-    return c.json({
-      success: true,
-      data: result?.rows,
-    });
-  } catch (e: any) {
-    return c.json({
-      success: false,
-      data: null,
-      msg: `!server error. ${e?.message}`,
-    });
-  }
-});
-
-router.post("/update_memo", async (c) => {
+router.post("/upsert_memo", async (c) => {
   try {
     const body = await c.req.parseBody();
     const id = Number(body["id"] || 0);
     const title = String(body["title"] || "");
     const content = String(body["content"] || "");
     const nickname = String(body["nickname"] || "");
-    const result = await query(
-      `
+    let result: any = {};
+    if (id > 0) {
+      result = await query(
+        `
     UPDATE t_memo SET
     title=$1,
     content=$2,
     nickname=$3
     WHERE id=$4
     `,
-      [title, content, nickname, id],
-    );
-    console.log(`#result:`, result);
+        [title, content, nickname, id],
+      );
+      console.log(`#result:`, result);
+    } else {
+      result = await query(
+        `
+    INSERT INTO t_memo(title,content,nickname)
+    VALUES($1,$2,$3)
+    `,
+        [title, content, nickname],
+      );
+      console.log(`#result:`, result);
+    }
 
     return c.json({
       success: true,
