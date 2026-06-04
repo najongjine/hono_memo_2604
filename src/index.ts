4268bc3ff6071a2502a16feddc9e92089dba66d8
najongjine <najongjin3@hotmail.com>
2026-06-04 10:08:22 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
